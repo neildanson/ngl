a3ddc291581d7e213e6e3a78dd4386726c9c21d3
Neil Danson <neil.danson@gmail.com>
2023-07-12 11:02:22 +0100

    fmt

--- src/parser_combinator.rs
+++ src/parser_combinator.rs
@@ -2,18 +2,17 @@ use std::rc::Rc;
 
 #[derive(Debug, PartialEq)]
 pub struct ParserState<'a> {
-    remaining : &'a str,
-    position : usize, //TODO add line numbers
+    remaining: &'a str,
+    position: usize, //TODO add line numbers
 }
 
 impl<'a> ParserState<'a> {
-    fn new (input: &'a str) -> Self {
+    fn new(input: &'a str) -> Self {
         Self {
             remaining: input,
             position: 0,
         }
     }
-
 
     fn advance(&self, n: usize) -> Self {
         Self {
@@ -23,12 +22,11 @@ impl<'a> ParserState<'a> {
     }
 }
 
-impl<'a> From <&'a str> for ParserState<'a> {
+impl<'a> From<&'a str> for ParserState<'a> {
     fn from(input: &'a str) -> Self {
         Self::new(input)
     }
 }
-
 
 pub trait Parser<'a> {
     type Output;
@@ -36,36 +34,43 @@ pub trait Parser<'a> {
 }
 
 struct ParserFn<'a, Output> {
-    parser: Rc<dyn Fn (ParserState<'a>) -> Result<(Output, ParserState<'a>), String>>,
+    parser: Rc<dyn Fn(ParserState<'a>) -> Result<(Output, ParserState<'a>), String>>,
 }
 
-impl<'a,  Output> ParserFn<'a,  Output>  {
-    pub fn new(parser: Rc<dyn Fn(ParserState<'a>) -> Result<(Output, ParserState<'a>), String>>) -> Self {
+impl<'a, Output> ParserFn<'a, Output> {
+    pub fn new(
+        parser: Rc<dyn Fn(ParserState<'a>) -> Result<(Output, ParserState<'a>), String>>,
+    ) -> Self {
         Self { parser }
     }
 }
 
-impl <'a,  Output> Parser<'a> for ParserFn<'a,  Output>{
+impl<'a, Output> Parser<'a> for ParserFn<'a, Output> {
     type Output = Output;
     fn parse(&self, input: ParserState<'a>) -> Result<(Output, ParserState<'a>), String> {
         (self.parser)(input)
     }
 }
 
-fn format_error<T, U>(expected : T, actual : U, state : &ParserState) -> String 
-    where T: std::fmt::Display,
-     U: std::fmt::Display {
-    format!("Expected '{}' but got '{}' at {}", expected, actual, state.position)
+fn format_error<T, U>(expected: T, actual: U, state: &ParserState) -> String
+where
+    T: std::fmt::Display,
+    U: std::fmt::Display,
+{
+    format!(
+        "Expected '{}' but got '{}' at {}",
+        expected, actual, state.position
+    )
 }
 
-pub fn pchar<'a>(c:char) -> impl Parser <'a, Output = char> {
+pub fn pchar<'a>(c: char) -> impl Parser<'a, Output = char> {
     ParserFn::new(Rc::new(move |state: ParserState| {
         let mut chars = state.remaining.chars();
         match chars.next() {
             Some(letter) if letter == c => {
                 let parser_state = state.advance(1);
                 Ok((c, parser_state))
-            },
+            }
             Some(letter) => Err(format_error(c, letter, &state)),
             None => Err(format_error(c, "EOF", &state)),
         }
@@ -78,7 +83,6 @@ mod tests {
     #[test]
     fn test_pchar_eof() {
         let h_parser = pchar('H');
-
         let result = h_parser.parse("".into());
         let expected = Err("Expected 'H' but got 'EOF' at 0".to_string());
         assert_eq!(result, expected);
@@ -87,7 +91,6 @@ mod tests {
     #[test]
     fn test_pchar_wrong_letter() {
         let h_parser = pchar('H');
-
         let result = h_parser.parse("c".into());
         let expected = Err("Expected 'H' but got 'c' at 0".to_string());
         assert_eq!(result, expected);
@@ -96,9 +99,14 @@ mod tests {
     #[test]
     fn test_pchar_success() {
         let h_parser = pchar('H');
-
         let result = h_parser.parse("H".into());
-        let expected = Ok(('H', ParserState { remaining: "", position: 1 }));
+        let expected = Ok((
+            'H',
+            ParserState {
+                remaining: "",
+                position: 1,
+            },
+        ));
         assert_eq!(result, expected);
     }
 }
