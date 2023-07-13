e158eeb508e623221c6d75400f27bf8950ccdb9c
Neil Danson <neil.danson@gmail.com>
2023-07-13 15:40:22 +0100

    Add a microbenchmark

---
+++ benches/benchmark.rs
@@ -0,0 +1,21 @@
+extern crate ngl;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use ngl::parser_combinator::*;
+use ngl::*;
+
+fn parse_success(c: &mut Criterion) {
+    let truthy_parser = por!(pchar!('t'), pchar!('f'));
+
+    c.bench_function("Parse Success", |b| {
+        b.iter(|| {
+            for _ in 0..100 {
+                let _ = black_box(truthy_parser("t".into()));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, parse_success);
+criterion_main!(benches);
