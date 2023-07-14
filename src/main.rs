2ef3b9b23d725b16e6c74fb138f685e97d2ead95
Neil Danson <neil.danson@gmail.com>
2023-07-14 09:47:50 +0100

    Update Example

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ mod parser_combinator;
 use parser_combinator::*;
 
 fn main() {
-    let parser = pthen!(por!(pchar!('H'), pchar!('h')), pchar!('e'));
-    let result = parser("hello".into());
+    let parser = pthen!(
+        poptional!(pchar!('\n')),
+        pthen!(por!(pchar!('H'), pchar!('h')), pchar!('e'))
+    );
+    let result = parser("\nhello".into());
     println!("{:?}", result);
 }
