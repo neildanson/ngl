4db915607d1ba882121a4560e9989a59748ea5ce
Neil Danson <neil.danson@gmail.com>
2023-07-18 10:18:33 +0100

    Added benchmark (will be good comparison with my other parser_combinator project)

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -51,6 +51,29 @@ fn parse_string_fail(c: &mut Criterion) {
     });
 }
 
+fn parse_int_success(c: &mut Criterion) {
+    let any_number = pany!('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+    let many_numbers = any_number.many1();
+    let number_parser = pthen(poptional(pchar('-')), many_numbers);
+
+    let to_number = number_parser.map(move |(negate, value): (Option<char>, Vec<char>)| {
+        let string: String = value.into_iter().collect();
+        let number = string.parse::<i32>().unwrap();
+        match negate {
+            Some(_) => -number,
+            None => number,
+        }
+    });
+
+    c.bench_function("Parse Success", |b| {
+        b.iter(|| {
+            for _ in 0..100 {
+                let _ = black_box(to_number.parse("-123456789"));
+            }
+        })
+    });
+}
+
 criterion_group!(
     benches,
     parse_char_success,
