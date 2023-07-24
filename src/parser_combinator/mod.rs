ba80869351d8ab44de25e7893f1d19dd80aef251
Neil Danson <neil.danson@gmail.com>
2023-07-24 10:35:13 +0100

    pull out tests to seperate module

--- src/parser_combinator/mod.rs
+++ src/parser_combinator/mod.rs
@@ -1,5 +1,6 @@
 pub mod continuation;
 pub mod error;
+#[macro_use]
 pub mod parser;
 pub mod token;
 
@@ -7,3 +8,6 @@ pub use continuation::*;
 pub use error::*;
 pub use parser::*;
 pub use token::*;
+
+#[cfg(test)]
+pub mod tests;
