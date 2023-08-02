aa079350eb524b8713e771216174d90a98ab24f9
Neil Danson <neil.danson@gmail.com>
2023-08-02 16:47:36 +0100

    "stress test" (haha)

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,36 @@ use ngl::parser_combinator::*;
 use ngl::untyped_language::*;
 
 fn main() {
-    let fun_binding = pfun();
+    let fun_binding = pmany(pfun());
+
+    let start = std::time::Instant::now();
 
     let result = fun_binding.parse(
         "fun name(param: type, paramx: typex) {
             let x = 1;
             let y = 2;
-        }"
+        }
+        fun name(param: type, paramx: typex) {
+            let x = 1;
+            let y = 2;
+        }
+        fun name(param: type, paramx: typex) {
+            let x = 1;
+            let y = 2;
+        }
+        fun name(param: type, paramx: typex) {
+            let x = 1;
+            let y = 2;
+        }
+        fun name(param: type, paramx: typex) {
+            let x = 1;
+            let y = 2;
+        }
+        "
         .into(),
     );
 
-    //let result = let_binding.parse("let x = true;".into());
+    let end = std::time::Instant::now();
 
-    println!("{:#?}", result);
+    println!("{:#?} \n\nTook {:?}", result, (end - start));
 }
